a5cfb293cf242eeb7961cebbb2a8bc0549a233bf
Jake Wenzel <rwenzel0@email.cpcc.edu>
2018-12-19 00:23:48 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <navbar-component></navbar-component>
+    <div class="container">
+      <page-heading-component></page-heading-component>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import NavbarComponent from '@/components/NavbarComponent.vue'
+import PageHeadingComponent from '@/components/PageHeadingComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavbarComponent,
+    PageHeadingComponent
+  }
+}
+</script>
---
+++ src/components/PageHeadingComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1 class="text-center">{{title}}</h1>
+</template>
+
+<script>
+export default {
+  name: 'PageHeadingComponent',
+  data () {
+    return {
+      title: 'Daily Videos'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
